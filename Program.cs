6afe4bf2eec587e049ac7e199d92b569797b3ae6
VINICIUS <marvinicius78910@gmail.com>
2020-11-18 20:56:30 -0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deseja ver a tabuada de qual número ?");
+            int resposta = int.Parse(Console.ReadLine());
+            
+            int resultado;
+            
+            for (int contador = 0; contador <=10; contador++)        
+            {
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado}");   
+            }
+        }
+    }
+}
